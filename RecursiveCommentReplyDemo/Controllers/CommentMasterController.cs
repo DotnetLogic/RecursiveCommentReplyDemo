3be67d1a2f6b60cbc31c3c472c6ae60a81d19e41
DotnetLogic <dsachin024@gmail.com>
2024-07-27 13:38:55 +0530

    Recusive implementation for parent to n child call

---
+++ RecursiveCommentReplyDemo/Controllers/CommentMasterController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RecursiveCommentReplyDemo.Model;
+
+namespace RecursiveCommentReplyDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentMasterController : ControllerBase
+    {
+        [HttpGet]
+        [Route("getall-commentReply")]
+        public async Task<IActionResult> GetAllCommentReply()
+        {
+            List<Comment> mainResult = new();
+            List<Comment> subtList = new();
+            List<Comment> commentList = new();
+            List<Comment> allList = new();
+
+            //Get all comment : Means parentId is null
+            commentList = Comment.GetAllCommentReply().Where(x => x.ParentId == null).ToList();
+
+            foreach (var item in commentList)
+            {
+                allList = Comment.GetAllCommentReply().ToList();
+
+                subtList = commentList.Where(x => x.CommentId == item.CommentId).Select(x => new Comment()
+                {
+                    CommentId = x.CommentId,
+                    CommentReply = x.CommentReply,
+                    ParentId = x.ParentId,
+                    ReplyList = GetAllReplies(ref allList, x.CommentId)
+                }
+                    ).ToList();
+                mainResult.AddRange(subtList);
+            }
+            return StatusCode(200, mainResult);
+        }
+        public static List<Comment> GetAllReplies(ref List<Comment> comments, long parentId)
+        {
+            List<Comment> innerCategories = comments;
+            return comments.Where(x => x.ParentId == parentId).Select(m => new Comment()
+            {
+                CommentId = m.CommentId,
+                CommentReply = m.CommentReply,
+                ParentId = parentId,
+                ReplyList = innerCategories.Where(x => x.ParentId == m.CommentId).Count() > 0 ? GetAllReplies(ref innerCategories, m.CommentId) : null
+            }).ToList();
+
+        }
+
+    }
+}
---
+++ RecursiveCommentReplyDemo/Model/Comment.cs
@@ -0,0 +1,31 @@
+﻿namespace RecursiveCommentReplyDemo.Model
+{
+    public class Comment
+    {
+        public long CommentId { get; set; }
+        public string CommentReply { get; set; }
+        public long? ParentId { get; set; }
+        public List<Comment> ReplyList { get; set; }
+        public static List<Comment> GetAllCommentReply()
+        {
+            return new List<Comment>()
+            {
+                new Comment(){CommentId=1,CommentReply="First Comment1",ParentId=null},
+                new Comment(){CommentId=2,CommentReply="Second Comment",ParentId=null},
+                new Comment(){CommentId=3,CommentReply="Third Comment1",ParentId=null},
+
+                 new Comment(){CommentId=4,CommentReply="Comment1.1",ParentId=1},
+                 new Comment(){CommentId=5,CommentReply="Second Comment1.1.2",ParentId=4},
+                 new Comment(){CommentId=6,CommentReply="Third Comment1.1.2.3",ParentId=5},
+
+                 new Comment(){CommentId=7,CommentReply="Second Comment2.1",ParentId=2},
+                 new Comment(){CommentId=8,CommentReply="Second Comment2.2",ParentId=2},
+                 new Comment(){CommentId=9,CommentReply="Third Comment3.1",ParentId=3},
+
+                 new Comment(){CommentId=10,CommentReply="First Comment2.2",ParentId=7},
+                 new Comment(){CommentId=11,CommentReply="Second Comment2.3",ParentId=10},
+                 new Comment(){CommentId=12,CommentReply="Third Comment2.4",ParentId=10},
+            };
+        }
+    }
+}
